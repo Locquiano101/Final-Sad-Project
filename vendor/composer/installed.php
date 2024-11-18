8d4325fabb06520957cb97194329f77a865c26e6
is.locquiano <locquianopatrickgreg@gmail.com>
2024-11-19 07:56:40 +0800

    trying php

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'penromis/greg',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'fe1cb02ede490f3fc3a0c61bdd3de5c402fdf246',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'penromis/greg' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'fe1cb02ede490f3fc3a0c61bdd3de5c402fdf246',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
