ca6061071f674da71c4fccd0b346d2558380628a
is.locquiano <locquianopatrickgreg@gmail.com>
2024-11-22 20:32:52 +0800

    tset

--- client-dashboard/upload.php
+++ client-dashboard/upload.php
@@ -1,4 +1,9 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
+
+
 // Enable error reporting for debugging
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
