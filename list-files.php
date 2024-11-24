ca286cd0b73dd4598408cef2a02992c5af3f4c2a
is.locquiano <locquianopatrickgreg@gmail.com>
2024-11-24 09:04:49 +0800

    ASASDSDA

--- list-files.php
+++ list-files.php
@@ -2,7 +2,8 @@
 header('Content-Type: application/json');
 
 // Directory where files are stored
-$storageDir = 'storage';
+$storageDir = __DIR__ . DIRECTORY_SEPARATOR . `uploads/Brian_Joshua_V___Gania/documents/`;
+
 
 if (!is_dir($storageDir)) {
     echo json_encode(['status' => 'error', 'message' => 'Storage directory not found']);
